65bec0e83b6ed3cad1f33a849a91f70d10ded698
wnsdnn <a24369029@gmail.com>
2022-06-11 15:25:11 +0900

    mvc 작성 완료

---
+++ src/Controller/View.php
@@ -0,0 +1,12 @@
+<?php
+namespace src\Controller;
+
+class View
+{
+    function main() {
+        view("main");
+    }
+}
+
+
+
---
+++ src/DB.php
@@ -0,0 +1,34 @@
+<?php
+namespace src\Controller;
+
+class DB
+{
+    static $db = null;
+    static function get()
+    {
+        if(is_null(self::$db)) {
+            $option = [ 19 => 5, 3 => 2 ];
+
+            self::$db = new \PDO("mysql:host=localhost; dbname= ; charset=utf8mb4", "root", "", $option);
+        }
+        return self::$db;
+    }
+}
+
+
+function execute($sql, $data = []) {
+    $q = DB::get()->prepare($sql);
+    return $q->exeucte($data);
+};
+
+function fetch($sql, $data = []) {
+    $q = DB::get()->prepare($sql);
+    $q->exeucte($data);
+    return $q->fetch(); 
+};
+
+function fetchAll($sql, $data = []) {
+    $q = DB::get()->prepare($sql);
+    $q->exeucte($data);
+    return $q->fetchAll(); 
+};
